73317716dad3d2e31eb158550b58a389332525df
nasrin jafari <nasrin.jafari770@gmail.com>
2023-10-06 01:38:04 +0330

    upload cv on website

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,10 @@
-import CV from "../../assets/cv.pdf";
+import nasrin_jafari_cv from "../../assets/nasrin_jafari_cv.pdf";
 import "./cta.scss";
 import { Link } from "react-router-dom";
 const CTA = () => {
   return (
     <div className="cta">
-      <a className="btn" href={CV} download>
+      <a className="btn" href={nasrin_jafari_cv} download>
         Download CV
       </a>
       <Link className="btn btn-primary" to="/contact">
