90d00147167a25598d558a63256de7431b9b533f
nasrin jafari <nasrin.jafari770@gmail.com>
2023-10-06 01:05:42 +0330

    edit import cta

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as CTA } from "./cta/CtA";
+export { default as CTA } from "./cta/CTA";
 export { default as NavBar } from "./navbar/NavBar";
 export { default as PageHeaderContent } from "./pageHeaderContent/index";
