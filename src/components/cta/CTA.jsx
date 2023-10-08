05cf33b4752bc937980363243aa44f8046b857d5
nasrin jafari <nasrin.jafari770@gmail.com>
2023-10-08 07:37:49 +0330

    change name file cv

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,10 @@
-import Nasrin_Jafari from "../../assets/Nasrin_Jafari.pdf";
+import cv_nasrinJafari from "../../assets/cv_nasrinJafari.pdf";
 import "./cta.scss";
 import { Link } from "react-router-dom";
 const CTA = () => {
   return (
     <div className="cta">
-      <a className="btn" href={Nasrin_Jafari} download>
+      <a className="btn" href={cv_nasrinJafari} download>
         Download CV
       </a>
       <Link className="btn btn-primary" to="/contact">
