e4c660851a94274a8ca6c3b71949c61fb456d81a
nasrin jafari <nasrin.jafari770@gmail.com>
2023-10-20 23:28:19 +0330

    edit src import resume

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const CTA = () => {
   return (
     <div className="cta">
-      <a className="btn" href={'../../assets/cv-nasrinJafari.pdf'} download>
+      <a className="btn" href={'/cv-nasrinJafari.pdf'} download>
         Download CV
       </a>
       <Link className="btn btn-primary" to="/contact">
